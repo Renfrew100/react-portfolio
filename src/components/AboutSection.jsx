7a54109f29e5b69fd34d5b3fe3b23b7e3869e0d0
Ali Muhammad <aliaftab9@hotmail.com>
2025-09-01 10:52:59 -0400

    hero+about section

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -12,20 +12,16 @@ export const AboutSection = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
           <div className="space-y-6">
             <h3 className="text-2xl font-semibold">
-              Passionate Web Developer & Tech Creator
+              Western University MEng Graduate April 2025
             </h3>
-
+            <h1>Founder at Space Fusion Inc.</h1>
             <p className="text-muted-foreground">
-              With over 5 years of experience in web development, I specialize
-              in creating responsive, accessible, and performant web
-              applications using modern technologies.
+              Ali, a recent graduate from Master of Engineering at Western specializes in 
+              frontend, backend, and server side development 
             </p>
 
             <p className="text-muted-foreground">
-              I'm passionate about creating elegant solutions to complex
-              problems, and I'm constantly learning new technologies and
-              techniques to stay at the forefront of the ever-evolving web
-              landscape.
+             I'm passionite about space, travel, and sports.
             </p>
 
             <div className="flex flex-col sm:flex-row gap-4 pt-4 justify-center">
@@ -67,7 +63,7 @@ export const AboutSection = () => {
                   <h4 className="font-semibold text-lg">UI/UX Design</h4>
                   <p className="text-muted-foreground">
                     Designing intuitive user interfaces and seamless user
-                    experiences.
+                    experiences, using key tools such as, Figma.
                   </p>
                 </div>
               </div>
@@ -82,7 +78,7 @@ export const AboutSection = () => {
                   <h4 className="font-semibold text-lg">Project Management</h4>
                   <p className="text-muted-foreground">
                     Leading projects from conception to completion with agile
-                    methodologies.
+                    methodologies, 
                   </p>
                 </div>
               </div>
